dbb11381542999348f094f167fb872a9bb22e372
Sean <sean.haugen560@gmail.com>
2023-10-11 07:54:57 -0500

    UPDATE

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(`MongoDB connection error: ${error}`));
+db.once("open", () => console.log("Connected to MongoDB"));
