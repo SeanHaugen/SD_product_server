c5856e4dc88e8b4f1e4aae0abca4768e4622e099
Sean <sean.haugen560@gmail.com>
2023-10-12 08:18:42 -0500

    update

--- models/promoCollection.js
+++ models/promoCollection.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose");
 
 // Define a schema for your collection
 const promoItemSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   Item_Number: Number,
   Name: String,
   Category: String,
   SubCategory: String,
-  Page: Number,
   Description: String,
   Keywords: String,
   Colors: String,
@@ -15,23 +13,10 @@ const promoItemSchema = new mongoose.Schema({
   Product_Height_Inches: String,
   Produce_Depth_Inches: String,
   Pricing_Group: String,
-  Qty1: Number,
-  Prc1: Number,
-  Qty2: Number,
-  Prc2: Number,
-  Qty3: Number,
-  Prc3: Number,
-  Qty4: Number,
-  Prc4: Number,
-  Qty5: Number,
-  Prc5: Number,
-  Qty6: Number,
-  Prc6: Number,
   SetupChg: Number,
   Artwork_Required: String,
   Prop65_Status: String,
   FR_Rating: String,
-  ANSI_BIFMA: String,
   Package_Size: String,
   Package_Weight: String,
   Product_Weight: String,
@@ -45,6 +30,6 @@ const promoItemSchema = new mongoose.Schema({
 });
 
 // Create a model based on the schema
-const promoItemModel = mongoose.model("promoitems", promoItemSchema);
+const promoItemModel = mongoose.model("promos", promoItemSchema);
 
 module.exports = promoItemModel;
--- server.js
+++ server.js
@@ -232,14 +232,23 @@ app.get("/mediaspecs", async (req, res) => {
   }
 });
 
-app.get("/promoitems", async (req, res) => {
-  try {
-    const promoItems = await promoItemModel.find({}); // Find all items in the collection
+app.get("/promo-items", async (req, res) => {
+  //   try {
+  //     const promoItems = await promoItemModel.find({}); // Find all items in the collection
 
+  //     res.json(promoItems);
+  //   } catch (error) {
+  //     console.error("Error fetching promo items:", error); // Log the error for debugging
+  //     res.status(500).json({ message: "Internal server error" });
+  //   }
+
+  try {
+    const promoItems = await promoItemModel.find({});
+    console.log(promoItems); // Add this line to log the results
     res.json(promoItems);
   } catch (error) {
-    console.error("Error fetching promo items:", error); // Log the error for debugging
-    res.status(500).json({ message: "Internal server error" });
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
   }
 });
 
@@ -300,6 +309,94 @@ app.put("/update/pricing/:itemNumber", async (req, res) => {
   } catch (error) {
     console.error("Error updating item", error);
     res.status(500).json({ message: "Could not update item", error });
+  }
+});
+
+app.post("/add/promo-items", async (req, res) => {
+  try {
+    const newItemData = req.body; // This should contain the selected item data
+    const newPromoItem = new promoItemModel(newItemData);
+    await newPromoItem.save();
+    console.log(newPromoItem);
+    res.status(201).json(newPromoItem);
+  } catch (error) {
+    console.error("Error adding promo item:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/add", async (req, res) => {
+  try {
+    const { Item_Number, Name, Description, Keywords, Category, SubCategory } =
+      req.body;
+
+    // Validate input data (you can add more validation logic here)
+    if (!Item_Number || !Name || !Category) {
+      return res.status(400).json({ message: "Required fields missing" });
+    }
+
+    const newItem = new items({
+      Item_Number,
+      Name,
+      Description,
+      Keywords,
+      Category,
+      SubCategory,
+      Colors,
+      Product_Width_inches,
+      Product_Height_Inches,
+      Produce_Depth_Inches,
+      Artwork_Required,
+      Package_Size,
+      Package_Weight,
+      Product_Weight,
+      Kit_Includes,
+      Materials,
+      Imprint_Method,
+      Origin,
+    });
+
+    await newItem.save();
+    res.status(201).json({ message: "Item added successfully" });
+  } catch (error) {
+    console.error("Error adding item:", error);
+    res
+      .status(500)
+      .json({ message: "Error adding item", error: error.message });
+  }
+});
+
+app.post("/pricingAdd", async (req, res) => {
+  try {
+    console.log("Received request body:", req.body);
+    const { Item_Number, Name, Pricing } = req.body;
+    // Check if an item with the given Item_Number already exists
+    let existingItem = await PricingModel.findOne({ Item_Number });
+
+    if (existingItem) {
+      return res.status(400).json({ message: "Item already exists" });
+    }
+
+    // Create a new item document
+    const newItem = new PricingModel({
+      Item_Number,
+      Name,
+      Pricing: Pricing.map((entry) => ({
+        label: entry.label,
+        prices: entry.prices,
+      })),
+    });
+
+    // Save the new item to the database
+    await newItem.save();
+
+    console.log("Item pricing added successfully");
+    res.status(201).json({ message: "Item pricing added successfully" });
+  } catch (error) {
+    console.error("Error adding item pricing:", error);
+    res
+      .status(500)
+      .json({ message: "Error adding item pricing", error: error.message });
   }
 });
 
