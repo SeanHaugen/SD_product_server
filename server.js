f2718213fe2240bf6060f03b7b4bb675d38db1b1
Sean <sean.haugen560@gmail.com>
2023-10-16 12:34:52 -0500

    updated

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const flatRateModel = require("./models/flatRateCollection");
 const InfoModel = require("./models/infoCollection");
 const mediaModel = require("./models/mediaCollection");
 const promoItemModel = require("./models/promoCollection");
-const additionalInfoModel = require("./models/additionalInfoCollection");
+const additionalInfoModel = require("./models/addtionalInfoCollection");
 
 DATABASE_PASSWORD = "DkD0ml96WSM62TAn";
 DATABASE = `mongodb+srv://seanhaugen560:${DATABASE_PASSWORD}@cluster0.adhrbht.mongodb.net/products?retryWrites=true&w=majority`;
