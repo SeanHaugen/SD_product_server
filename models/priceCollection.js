fa09924957f41b7f42232204e8bed68482a482cd
Sean <sean.haugen560@gmail.com>
2023-10-16 08:19:02 -0500

    model update

--- models/priceCollection.js
+++ models/priceCollection.js
@@ -3,27 +3,13 @@ const mongoose = require("mongoose");
 //Pricing
 
 const PricingSchema = new mongoose.Schema({
-  // url: String,
-  // Name: String,
-  // Item_Number: String,
-  // Promo: Boolean,
-  // Pricing: [
-  //   [String, String, String, String, String],
-  //   [String, String, String, String, String],
-  // ],
-
   url: String,
   Name: String,
   Item_Number: String,
-  Internal_Info: String,
+  Promo: Boolean,
   Pricing: [
-    {
-      type: {
-        type: String,
-        enum: ["Retail", "Net", "CPP1CS", "KEY2CS", "INP3CS"],
-      },
-      prices: [String],
-    },
+    [String, String, String, String, String],
+    [String, String, String, String, String],
   ],
 });
 
