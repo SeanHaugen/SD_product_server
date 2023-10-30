f41d1816d834c73f30f9ba08d229bdd2264c0acd
Sean <sean.haugen560@gmail.com>
2023-10-30 10:31:44 -0500

    update

--- models/userCollection.js
+++ models/userCollection.js
@@ -5,6 +5,6 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
-const UserModel = mongoose.model("users", userSchema, "users");
+const UserModel = mongoose.model("User", userSchema, "users");
 
 module.exports = UserModel;
