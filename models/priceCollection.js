4219e5560714f6e9761e5a17d642df675d36abb0
Sean <sean.haugen560@gmail.com>
2024-01-08 11:15:10 -0600

    update

--- models/priceCollection.js
+++ models/priceCollection.js
@@ -1,18 +1,10 @@
-const mongoose = require("mongoose");
-
-//Pricing
-
 const PricingSchema = new mongoose.Schema({
   url: String,
   Name: String,
   Item_Number: String,
   Promo: Boolean,
-  Pricing_Table: [
+  Pricing: [
     [mongoose.Schema.Types.Mixed], // Use mongoose.Schema.Types.Mixed for the inner arrays
     [mongoose.Schema.Types.Mixed],
   ],
 });
-
-const PricingModel = mongoose.model("itempricing", PricingSchema);
-
-module.exports = PricingModel;
