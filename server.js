98822408c1ae02535e9a1476fc2726e89bb9a36b
Sean <sean.haugen560@gmail.com>
2024-05-09 08:30:52 -0500

    update to prev version

--- server.js
+++ server.js
@@ -1,73 +1,29 @@
-//dependencies import
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const axios = require("axios");
 
-//Model Imports
 const itemsModel = require("./models/itemsCollection");
 const PricingModel = require("./models/priceCollection");
+const EurofitModel = require("./models/eurofitCollection");
+const flatRateModel = require("./models/flatRateCollection");
+const InfoModel = require("./models/infoCollection");
 const mediaModel = require("./models/mediaCollection");
 const promoItemModel = require("./models/promoCollection");
+const UserModel = require("./models/userCollection");
+const adherenceModel = require("./models/adherence");
 
-//controller imports
-const {
-  register,
-  login,
-  protected,
-} = require("./controllers/login/authController");
-
+//controller impports
+const { register, login } = require("./controllers/login/authController");
 const {
   getDashboardData,
 } = require("./controllers/dashboard/dashboardController");
 
-const {
-  postUserNotes,
-  getUserNotes,
-  putUserNotes,
-  deleteUserNotes,
-} = require("./controllers/userControl/userNotesControllers");
-
-const {
-  getSubcategoryItems,
-  getSubCategory,
-  getItemCategory,
-  getCategory,
-} = require("./controllers/products/productCategoryController");
-
-const { getItemData } = require("./controllers/itemInfo/itemDataController");
-
-const {
-  getSearchResults,
-} = require("./controllers/itemInfo/itemSearchController");
-
-const {
-  getItemPricing,
-} = require("./controllers/itemInfo/itemPricingController");
-
-const {
-  getAdditionalInfo,
-} = require("./controllers/itemInfo/additionalInfoController");
-
-const {
-  getFlatRate,
-} = require("./controllers/itemInfo/itemFlatRateController");
-
-const {
-  getEurofitInfo,
-} = require("./controllers/itemInfo/eurofitItemController");
-
-const {
-  getMediaSpecs,
-  getSpecsForMedia,
-} = require("./controllers/itemInfo/mediaSpecsController");
-
-const {
-  getSimilarItems,
-} = require("./controllers/itemInfo/similarItemsController");
-
-//initialize as express application
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // First database connection for products
 const productDBPassword = "DkD0ml96WSM62TAn";
@@ -77,9 +33,17 @@ const productDatabase = `mongodb+srv://seanhaugen560:${productDBPassword}@cluste
 const dashboardDBPassword = "DkD0ml96WSM62TAn";
 const dashboardDatabase = `mongodb+srv://seanhaugen560:${dashboardDBPassword}@cluster0.adhrbht.mongodb.net/dashboard`;
 
-//initialize cors and express
 app.use(cors());
 app.use(express.json());
+
+// mongoose.connect(DATABASE, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// const db = mongoose.connection;
+// db.on("error", (error) => console.error(`MongoDB connection error: ${error}`));
+// db.once("open", () => console.log("Connected to MongoDB"));
 
 // Connect to the products database
 mongoose.connect(productDatabase, {
@@ -87,7 +51,6 @@ mongoose.connect(productDatabase, {
   useUnifiedTopology: true,
   dbName: "products",
 });
-
 const productDB = mongoose.connection;
 productDB.on("error", (error) =>
   console.error(`Products MongoDB connection error: ${error}`)
@@ -100,46 +63,468 @@ mongoose.createConnection(dashboardDatabase, {
   useUnifiedTopology: true,
   dbName: "dashboard", // Specify the database name here
 });
-
 const dashboardDB = mongoose.connection;
 dashboardDB.on("error", (error) =>
   console.error(`Dashboard MongoDB connection error: ${error}`)
 );
 
+app.get("/dashboardData", getDashboardData());
+/////////////////////////////////////////
+//user authentication
+// Secret key for JWT
+const secretKey = process.env.SECRET_KEY || "w3jRWpyq";
+
+// Middleware to authenticate token
+function authenticateToken(req, res, next) {
+  const token = req.header("Authorization");
+
+  // Log the actual token value
+  console.log("Received token:", token);
+
+  if (!token) {
+    console.error("No token provided");
+    // return res.status(401).send("Access denied");
+  }
+
+  // jwt.verify(token, secretKey, (err, user) => {
+  //   if (err) {
+  //     console.error("Token verification error:", err.message);
+  //     return res.status(403).send("Invalid token");
+  //   }
+
+  //   req.user = user;
+
+  // });
+  next();
+}
+
 app.post("/login", login);
 app.post("/register", register);
-app.get("/protected", protected);
+
+// // Register route
+// app.post("/register", async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+
+//     // Validate user data
+//     if (!username || !password) {
+//       return res
+//         .status(400)
+//         .json({ error: "Please provide all required fields." });
+//     }
+
+//     // Hash the password
+//     const hashedPassword = await bcrypt.hash(password, 10);
+
+//     // Create a new user in the database
+//     const user = new UserModel({ username, password: hashedPassword });
+//     await user.save();
+
+//     return res.status(201).json({ message: "User registered successfully." });
+//   } catch (error) {
+//     console.error("Error during registration:", error.message);
+//     return res
+//       .status(500)
+//       .json({ error: "Registration failed. Please try again later." });
+//   }
+// });
+
+// app.post("/login", async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+
+//     // Find the user by username
+//     const user = await UserModel.findOne({ username });
+
+//     if (!user) {
+//       return res.status(401).json({ error: "Invalid credentials." });
+//     }
+
+//     // Compare the hashed password
+//     const isPasswordValid = await bcrypt.compare(password, user.password);
+
+//     if (!isPasswordValid) {
+//       return res.status(401).json({ error: "Invalid credentials." });
+//     }
+
+//     // Generate a JWT
+//     const token = jwt.sign({ userId: user._id }, secretKey, {
+//       expiresIn: "8h",
+//     });
+
+//     // Send the token in the "Authorization" header
+//     return res.header("Authorization", `Bearer ${token}`).json({ token });
+//   } catch (error) {
+//     return res
+//       .status(500)
+//       .json({ error: "Login failed. Please try again later." });
+//   }
+// });
+
+// Example protected route
+app.get("/protected", (req, res) => {
+  console.log(
+    "Received token in protected route:",
+    req.header("Authorization")
+  );
+  res.json({ message: "This is a protected route." });
+});
 
 //Allow users to take notes
-app.post("/notes/:username", postUserNotes);
-app.get("/notes/:username", getUserNotes);
-app.put("/notes/:userId/:currentPage", putUserNotes);
-app.delete("/notes/:username", deleteUserNotes);
+app.post("/notes/:username", async (req, res) => {
+  try {
+    const { username } = req.params;
+    const { note } = req.body; // Remove currentPage from destructuring
 
-app.get("/dashboardData", getDashboardData);
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Assuming user.notes is an array field in the user schema to store notes
+    // Assuming you want to add a note without specifying a page
+    user.notes.push({ content: note });
+    await user.save();
+
+    res.status(201).json({ message: "Note created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.get("/notes/:username", async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const notes = user.notes.map((note) => note.content);
+
+    res.json({ notes });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.put("/notes/:userId/:currentPage", authenticateToken, async (req, res) => {
+  try {
+    const { userId, currentPage } = req.params;
+    const { note } = req.body;
+
+    const user = await UserModel.findById(userId);
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const targetNote = user.notes.find((n) => n.page === currentPage);
+
+    if (!targetNote) return res.status(404).json({ message: "Note not found" });
+
+    targetNote.note = note;
+    await user.save();
+
+    res.json({ message: "Note updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.delete("/notes/:username", authenticateToken, async (req, res) => {
+  try {
+    const { username } = req.params;
+    const { noteContent } = req.body; // Assuming your frontend sends the note content
+
+    const user = await UserModel.findOne({ username });
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    // Find the index of the note with the specified content
+    const noteIndex = user.notes.findIndex(
+      (note) => note.content === noteContent
+    );
+
+    if (noteIndex === -1) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    // Remove the note at the specified index
+    user.notes.splice(noteIndex, 1);
+
+    await user.save();
+
+    res.json({ message: "Note deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 //Get requests from the ItemsCollections
-app.get("/category", getCategory);
-//Get the item category
-app.get("/category/:category", getItemCategory);
-//Get the subcategory
-app.get("/SubCategory", getSubCategory);
-//get the item
-app.get("/subCategory/:items", getSubcategoryItems);
-//Get items through Search
-app.get("/search", getSearchResults);
+// Get unique categories
+app.get("/category", async (req, res) => {
+  try {
+    const allCategories = await itemsModel.distinct("Category");
+    console.log("Categories:", allCategories); // Add this line to log the results
+    res.json(allCategories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
 
-//Below will get all the relevant item data
-//Gets all the base data for an individual item
-app.get("/items", getItemData);
-app.get("/pricing/:criteria/:item", getItemPricing);
-app.get("/info", getAdditionalInfo);
-app.get("/flatRates/:item", getFlatRate);
-app.get("/eurofits", getEurofitInfo);
+//Get the item category
+app.get("/category/:category", async (req, res) => {
+  try {
+    const productCategory = req.params.category.replace(/"/g, "");
+    const result = await itemsModel.aggregate([
+      { $match: { Category: productCategory } },
+      { $group: { _id: "$SubCategory" } },
+      { $sort: { _id: 1 } },
+    ]);
+    const subcategories = result.map((item) => item._id);
+    res.send(subcategories);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error!");
+  }
+});
+
+//Get the subcategory
+app.get("/SubCategory", async (req, res) => {
+  const allItems = await itemsModel.distinct("SubCategory");
+  res.send(allItems);
+});
+
+//get the item
+app.get("/subCategory/:items", async (req, res) => {
+  const productCategory = req.params.items;
+
+  try {
+    const products = await itemsModel
+      .find({ SubCategory: productCategory })
+      .sort({ Name: 1 });
+    res.send(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//Get items through Search
+app.get("/items", async (req, res) => {
+  const itemNumber = req.query.item;
+
+  if (!itemNumber) {
+    return res
+      .status(400)
+      .send("Item number is required in the query parameters.");
+  }
+
+  try {
+    // Log the request
+    console.log("Request:", req.query);
+
+    // Log all items in the collection
+    // const allItems = await itemsModel.find({});
+    // console.log("All Items in Collection:", allItems);
+
+    // Query the specific item
+    const item = await itemsModel.findOne({ Item_Number: itemNumber });
+
+    if (!item) {
+      return res.status(404).send("Item not found");
+    }
+
+    // Log the retrieved item
+    console.log("Retrieved Item:", item);
+
+    res.send(item);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/search", async (req, res) => {
+  const searchQuery = req.query.q;
+
+  try {
+    const numericQuery = parseFloat(searchQuery);
+    let results;
+
+    if (!isNaN(numericQuery)) {
+      results = await itemsModel.find({ Item_Number: numericQuery });
+    } else {
+      results = await itemsModel
+        .find(
+          {
+            $text: { $search: searchQuery },
+          },
+          {
+            score: { $meta: "textScore" },
+          }
+        )
+        .sort({ score: { $meta: "textScore" } });
+    }
+    res.json(results);
+  } catch (error) {
+    console.log("Error searching database");
+    res
+      .status(500)
+      .json({ error: "An error occurred while searching the database." });
+  }
+});
+
+app.get("/pricing/:criteria/:item", async (req, res) => {
+  try {
+    const criteria = req.params.criteria;
+    const itemValue = req.params.item;
+    let query = {};
+
+    // Define the query based on the criteria
+    if (criteria === "item_number") {
+      query.Item_Number = itemValue.trim();
+    } else if (criteria === "name") {
+      query.Name = itemValue.trim();
+    } else if (criteria === "url") {
+      query.url = itemValue.trim();
+    } else {
+      return res.status(400).json({ message: "Invalid search criteria" });
+    }
+
+    const pricingDoc = await PricingModel.findOne(query);
+    // const pricingDoc = await PricingModel.findOne({
+    //   Item_Number: pricingItem.trim(), // Trim whitespace including newlines
+    // });
+
+    if (!pricingDoc) {
+      return res.status(404).json({ message: "Pricing not found" });
+    }
+    console.log(pricingDoc.Pricing);
+
+    const pricingArrays = pricingDoc.Pricing; // Extract the Pricing arrays
+
+    res.json(pricingArrays);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//Get Internal Information
+app.get("/info", async (req, res) => {
+  try {
+    const itemInfo = req.query.item;
+    const info = await InfoModel.findOne({
+      Item_Number: itemInfo.trim(),
+    });
+    console.log(info);
+
+    if (!info) {
+      return res.status(404).json({ message: "Internal Info not found" });
+    }
+
+    res.json(info);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//get flatrates
+app.get("/flatRates/:item", async (req, res) => {
+  try {
+    const flatRateItem = req.params.item;
+    const rateInfo = await flatRateModel.find({
+      Item_Number: flatRateItem,
+      Service: { $in: ["GROUND SERVICE", "2DAY", "STANDARD OVERNIGHT"] },
+    });
+    console.log(rateInfo);
+    if (rateInfo.length === 0) {
+      return res.status(404).json({ message: "Internal Info not found" });
+    }
+    res.json(rateInfo);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//get Eurofit info
+app.get("/eurofits", async (req, res) => {
+  try {
+    const itemNumber = parseInt(req.query.item);
+
+    if (isNaN(itemNumber)) {
+      return res.status(400).json({ message: "Invalid Item_Number provided" });
+    }
+
+    const info = await EurofitModel.findOne({
+      Item_Number: itemNumber,
+    });
+
+    if (!info) {
+      return res.status(404).json({ message: "Eurofit Info not found!" });
+    }
+
+    console.log("Query:", info);
+
+    res.json(info);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 //get media info Specifications
-app.get("/mediaspecs", getMediaSpecs);
-app.get("/specsForMedia", getSpecsForMedia);
+app.get("/mediaspecs", async (req, res) => {
+  try {
+    const media = req.query.item;
+    const mediaInfo = await mediaModel.find({
+      Type: media,
+    });
+    console.log(mediaInfo);
+    res.json(mediaInfo);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+app.get("/specsForMedia", async (req, res) => {
+  try {
+    // Fetch all mediaspecs from the "mediaspecs" collection
+    const mediaspecs = await mediaModel.find();
+
+    if (!mediaspecs || mediaspecs.length === 0) {
+      return res.status(404).json({ error: "No mediaspecs found" });
+    }
+
+    res.json(mediaspecs);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 //Get similar products from items collection
-app.get("/document/:pattern", getSimilarItems);
+
+app.get("/document/:pattern", async (req, res) => {
+  try {
+    const paramPattern = req.params.pattern;
+
+    // Use Mongoose to find documents with names matching the provided pattern
+    const documents = await Document.find({
+      name: { $regex: paramPattern, $options: "i" }, // Case-insensitive regex search
+    });
+
+    res.json({ documents });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 //PUT request
 //Edit the description of the item
